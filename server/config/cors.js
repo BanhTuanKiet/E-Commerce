59062ce74c506e9d500683d7d6443f59ee062e5d
BanhTuanKiet <kiett5153@gmail.com>
2025-08-27 16:17:23 +0700

    deploy web

--- server/config/cors.js
+++ server/config/cors.js
@@ -3,10 +3,8 @@ const cors = require('cors')
 const corsConfig = cors({
     origin: [
         `http://localhost:3000`,
-        // 'http://localhost:2908`,'
-        // `https://web-js-68ea2.web.app`,
-        // `https://web-js-68ea2.firebaseapp.com`,
-        // `https://gateway-4ot3.onrender.com`
+        `https://e-commerce-8eeb6.web.app`,
+        `https://e-commerce-8eeb6.firebaseapp.com`
     ],
     methods: ['Get', 'POST', 'PUT', 'DELETE'],
     optionsSuccessStatus: 200,
